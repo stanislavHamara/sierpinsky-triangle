1a3fb425a478c392e23edaf325b15663c3d4a9ca
Stan Hamara <stan.hamara@red-gate.com>
2019-09-11 07:38:05 +0100

    Set event listeners for mouse

--- src/js/index.js
+++ src/js/index.js
@@ -3,4 +3,26 @@ import print from './geometry'
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
+let isPanning = false;
+
+canvas.addEventListener('mousedown', () => {
+    console.log('mousedown')
+    isPanning = true;
+})
+
+canvas.addEventListener('mouseup', () => {
+    console.log('mouseup')
+    isPanning = false;
+})
+
+canvas.addEventListener('mousemove', () => {
+    if (isPanning) {
+        console.log('mousemove')
+    }
+})
+
+canvas.addEventListener('wheel', () => {
+    console.log('wheel')
+})
+
 print()
