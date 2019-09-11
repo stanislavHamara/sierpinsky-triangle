380b2da247c7a999aaf0022581397fb478aed4e7
Stan Hamara <stan.hamara@red-gate.com>
2019-09-11 08:01:11 +0100

    Draw sierpinsky triangle

--- src/js/geometry.js
+++ src/js/geometry.js
@@ -1,3 +1,16 @@
+const drawSierpinskyTriangle = (ctx, x, y, size, depth) => {
+    if (depth === 0) {
+        drawTriangle(ctx, x, y, size)
+    } else {
+        depth--;
+        const halfSize = size / 2
+        drawSierpinskyTriangle(ctx, x, y, halfSize, depth)
+        drawSierpinskyTriangle(ctx, x + halfSize, y, halfSize, depth)
+        drawSierpinskyTriangle(ctx, x + halfSize / 2, y - getTriangleHeight(y, halfSize), halfSize, depth)
+
+    }
+}
+
 const drawTriangle = (ctx, x, y, size) => {
     ctx.beginPath()
 
@@ -10,10 +23,13 @@ const drawTriangle = (ctx, x, y, size) => {
     ctx.strokeStyle = '#666666';
     ctx.stroke();
 
+    ctx.fillStyle = "#666666";
+    ctx.fill();
+
     ctx.closePath();
 }
 
 
 const getTriangleHeight = (y, size) => size * (Math.sqrt(3) / 2)
 
-export default drawTriangle
+export default drawSierpinskyTriangle
--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,11 @@
-import drawTriangle from './geometry'
+import drawSierpinskyTriangle from './geometry'
 
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
 let isPanning = false;
 
-drawTriangle(ctx, 0, 600, 600)
+drawSierpinskyTriangle(ctx, 0, 600, 600, 6)
 
 canvas.addEventListener('mousedown', () => {
     console.log('mousedown')
