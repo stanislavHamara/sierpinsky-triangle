5cae12f787dbca7b67f2744b83eb2ff3a9800c76
Stan Hamara <stan.hamara@red-gate.com>
2019-09-11 08:19:35 +0100

    Implement simple panning

--- src/js/index.js
+++ src/js/index.js
@@ -4,26 +4,36 @@ const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
 let isPanning = false;
+let cursorOrigin = { x: 0, y: 0 }
+let triangleOrigin = { x: 0, y: canvas.width }
 
-drawSierpinskyTriangle(ctx, 0, 600, 600, 6)
+drawSierpinskyTriangle(ctx, triangleOrigin.x, triangleOrigin.y, canvas.height, 5)
 
-canvas.addEventListener('mousedown', () => {
-    console.log('mousedown')
-    isPanning = true;
-})
-
-canvas.addEventListener('mouseup', () => {
-    console.log('mouseup')
-    isPanning = false;
-})
-
-canvas.addEventListener('mousemove', () => {
-    if (isPanning) {
-        console.log('mousemove')
-    }
-})
-
+canvas.addEventListener('mousedown', startPanning)
+canvas.addEventListener('mouseup', stopPanning)
+canvas.addEventListener('mouseout', stopPanning)
+canvas.addEventListener('mousemove', pan)
 canvas.addEventListener('wheel', () => {
     console.log('wheel')
 })
 
+function startPanning(e) {
+    isPanning = true;
+    cursorOrigin = { ...cursorOrigin, x: e.clientX, y: e.clientY };
+}
+
+function stopPanning(e) {
+    isPanning = false;
+    const offsetX = parseInt(e.clientX - cursorOrigin.x);
+    const offsetY = parseInt(e.clientY - cursorOrigin.y);
+    triangleOrigin = { ...triangleOrigin, x: triangleOrigin.x + offsetX, y: triangleOrigin.y + offsetY };
+}
+
+function pan(e) {
+    if (isPanning) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        const offsetX = parseInt(e.clientX - cursorOrigin.x);
+        const offsetY = parseInt(e.clientY - cursorOrigin.y);
+        drawSierpinskyTriangle(ctx, triangleOrigin.x + offsetX, triangleOrigin.y + offsetY, canvas.height, 5);
+    }
+}
